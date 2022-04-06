d6f3256c2a0d8885376685b95a534a406667c76c
Denny Pearce <digital.y.coffee@gmail.com>
2022-04-07 00:49:54 +0300

    write sections for tesla landing, add styles to it, work with components props

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,34 @@ import Section from './Section';
 const Home = () => {
   return (
     <Container>
-      <Section />
+      <Section
+        title='Model S'
+        description='Order Online for Touchless Delivery'
+        bgImg='model-s.jpg'
+        leftBtnText='Custom order'
+        rightBtnText='Existing inventory'
+      />
+      <Section
+        title='Model Y'
+        description='Order Online for Touchless Delivery'
+        bgImg='model-y.jpg'
+        leftBtnText='Custom order'
+        rightBtnText='Existing inventory'
+      />
+      <Section
+        title='Model 3'
+        description='Order Online for Touchless Delivery'
+        bgImg='model-3.jpg'
+        leftBtnText='Custom order'
+        rightBtnText='Existing inventory'
+      />
+      <Section
+        title='Model X'
+        description='Order Online for Touchless Delivery'
+        bgImg='model-x.jpg'
+        leftBtnText='Custom order'
+        rightBtnText='Existing inventory'
+      />
     </Container>
   );
 };
